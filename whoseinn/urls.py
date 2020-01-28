b5ed500e52f631fa32065dd62085a8e9f8472a4e
lwazin <nolijndlovu@gmail.com>
2020-01-28 08:32:11 +0200

    No comment.. minor alteration

--- whoseinn/urls.py
+++ whoseinn/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from .views import Home as home, fourZeroFour
