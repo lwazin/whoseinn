eb51b72575c19343b77d7ebeacf5f957ad0ee15f
lwazin <nolijndlovu@gmail.com>
2020-01-28 08:34:02 +0200

    ...

--- whoseinn/urls.py
+++ whoseinn/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static, staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from .views import Home as home, fourZeroFour
