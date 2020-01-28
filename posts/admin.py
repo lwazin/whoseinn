3588cc7aa1c7c28369816736c5ed569fc8f690e2
lwazin <nolijndlovu@gmail.com>
2020-01-28 06:16:42 +0200

    Moving more reliable components of the app..

--- posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Accom, Image, Application, InternalMessage
-
-admin.site.register(Accom)
-admin.site.register(Application)
-admin.site.register(Image)
-admin.site.register(InternalMessage)
